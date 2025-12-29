b1ff552500214a9045aad629f43595377cb08b2f
alfutih <alfutih@chalmers.se>
2025-12-29 21:59:01 +0100

    Get references to all form elements
    in addition test the references with just console.log to avoid any misstake.

---
+++ contact.js
@@ -0,0 +1,47 @@
+const firstName= document.getElementById("firstName");
+
+const lastName= document.getElementById("lastName");
+
+const email= document.getElementById("email");
+
+const phone= document.getElementById("phone");
+
+const subject= document.getElementById("subject");
+
+const message= document.getElementById("message");
+
+const submit= document.querySelector(".submitButton");
+
+const clear= document.querySelector(".clearButton");
+
+firstName.addEventListener("click", function(){
+    console.log("test");
+})
+
+lastName.addEventListener("click", function(){
+    console.log("test");
+})
+
+email.addEventListener("click", function(){
+    console.log("test");
+})
+
+phone.addEventListener("click", function(){
+    console.log("test");
+})
+
+subject.addEventListener("click", function(){
+    console.log("test");
+})
+
+message.addEventListener("click", function(){
+    console.log("test");
+})
+
+submit.addEventListener("click", function(){
+    console.log("test");
+})
+
+clear.addEventListener("click", function(){
+    console.log("test");
+})
